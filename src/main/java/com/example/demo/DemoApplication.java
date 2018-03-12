12c87df4618ad3c1dca8f429e5f554727bec2dbc
zhongliang <king@wise2c.com>
2018-03-12 14:18:52 +0800

    add filter example

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.filters.DemoFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	//register filter
+	@Bean
+	public FilterRegistrationBean demoFilter() {
+		FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+		filterRegistrationBean.setFilter(new DemoFilter());
+		return filterRegistrationBean;
 	}
 }
--- src/main/java/com/example/demo/filters/DemoFilter.java
+++ src/main/java/com/example/demo/filters/DemoFilter.java
@@ -1,7 +1,52 @@
 package com.example.demo.filters;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by king on 2018/3/9.
  */
-public class DemoFilter {
+
+@WebFilter(urlPatterns = "/api/*")
+public class DemoFilter extends OncePerRequestFilter {
+    public static final String TOKEN_EXPIRED = "Authorization Token Expired";
+    public static final String INVALID_TOKEN = "Authorization Token Invalid";
+    private static final String MISSING_TOKEN = "Missing Authorization Token";
+    private static final String TOKEN_HEADER_PREFIX = "Authorization";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (shouldSkipUrl(request)) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String header = request.getHeader(TOKEN_HEADER_PREFIX);
+        if(StringUtils.isEmpty(header)) {
+            unauthorizedError(response, MISSING_TOKEN);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private void unauthorizedError(HttpServletResponse response, String msg) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(msg);
+    }
+
+    private boolean  shouldSkipUrl(HttpServletRequest request) {
+        String[]  skipUrl = {"/api/user/login","/api/auth"};
+        List<String> skipUrlList = Arrays.asList(skipUrl);
+        return skipUrlList.stream().anyMatch(url->request.getRequestURI().contains(url));
+    }
+
 }
